0786ce6c00ad68bcc80aed9e184609b3f2118f2b
LikeAJohny <LikeAJohny@users.noreply.github.com>
2023-11-12 14:04:32 +0100

    remove unnecessary import

--- schemas/shows.ts
+++ schemas/shows.ts
@@ -1,4 +1,4 @@
-import { defineArrayMember, defineField, defineType } from 'sanity';
+import { defineArrayMember, defineField } from 'sanity';
 
 const showItem = defineArrayMember({
   name: 'show',
